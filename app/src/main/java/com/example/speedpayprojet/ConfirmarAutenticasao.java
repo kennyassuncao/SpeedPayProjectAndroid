d6c7e6866e6c904b4cb4ec7c1951fb53a599955f
Kenny Assuncao <kennyassuncao>
2022-02-12 12:24:39 -0100

    Transacao QR a quase a funcionar com um pequeno erro, correção tela QR Code, tela inicial

---
+++ app/src/main/java/com/example/speedpayprojet/ConfirmarAutenticasao.java
@@ -0,0 +1,14 @@
+package com.example.speedpayprojet;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfirmarAutenticasao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmar_autenticasao);
+    }
+}
